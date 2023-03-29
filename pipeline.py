dccf7cba745aef50eab142a6a87d99713dc8259b
Ivan Ledesma <ivanarielledesma@gmail.com>
2023-03-29 07:26:02 -0300

    Upload pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 print(sys.argv)
 
+# Create variable why fuction is select principal argument entry 
+# example input: python3 pipeline.py 29-03-23, output: job finished 29-03-23
+
 day = sys.argv[1]
 
 # some interesting things with pandas
