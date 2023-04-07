9e19684e31829e33c9a080d0b468c95be2dbf9bf
Ivan Ledesma <ivanarielledesma@gmail.com>
2023-04-07 15:17:13 -0300

    Changes to avoid confusion

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,6 +2,6 @@ import subprocess
 
 def test_pipeline():
     # Specifies the full path to the file 'pipeline.py'if located outside the test directory
-    result = subprocess.run(['python', '/home/ivan/Escritorio/docker-postgresql-pipeline/pipeline.py', '29-03-23'], capture_output=True, text=True)
+    result = subprocess.run(['python', '/home/ivan/Escritorio/docker-postgresql-pipeline/example.py', '29-03-23'], capture_output=True, text=True)
     # Verify expected output
     assert result.stdout.strip() == 'job finished = 29-03-23'
